45af38db523ccf159facbd091f0d77445867ed7a
juanfer54 <153625649+juanfer54@users.noreply.github.com>
2023-12-15 09:09:45 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+# Creacion de variables
+
+# Numero Entero
+a = 34
+b = 55
+c = 66
+
+# Caracter
+caracter_1 = 'd'
+caracter_2 = 'f'
+caracter_3 = '1'
+
+# Decimal
+
+x = 5.0
+y = 6.0
+z = 7.0
+
+# Creacion de lista
+
+mi_lista = [1,2,3,4,5]
+
+# Modificacion de lista
+
+mi_lista.append(500)
+print(mi_lista)
+
+elemento_nuevo = 605
+mi_lista.insert(2,elemento_nuevo)
+nuevo_valor= 45
+mi_lista[1] = nuevo_valor
+print(mi_lista)
+
+#Eliminación de Elementos de la Lista:
+mi_lista.remove(3)
+mi_lista.pop(4)
+
+# Convierte numero_entero a tipo float.
+# Convierte numero_decimal a tipo int.
+# Convierte caracter a tipo str.
+
+a = float(a)
+x = int(x)
+caracter_2 = str(caracter_2)
+print(a)
+print(x)
+print(caracter_2)
+
+
+print(a, b, c, caracter_1, caracter_2,caracter_3, x,y,z)
